f0fb0f66e60d4dd9f574efee95d659b320752643
David Pfahler <pfahler@vrvis.at>
2019-12-10 15:54:28 +0100

    added stage 2 from d10

--- d10.py
+++ d10.py
@@ -1,6 +1,34 @@
 import math
 
 
+def getVisibleAsteroids(a, asteroids):
+    pos = {}
+    for b in asteroids:
+        if a == b:
+            pass
+        else:
+            rel = (a[0] - b[0], a[1] - b[1])
+            length = math.sqrt(rel[0] * rel[0] + rel[1] * rel[1])
+            e = (rel[0] / length, rel[1] / length)
+            rounded = (round(e[0], 6), round(e[1], 6))
+            if rounded not in pos:
+                pos[rounded] = (length, b)
+            elif pos[rounded][0] > length:
+                pos[rounded] = (length, b)
+
+    return [v[1] for v in pos.values()]
+
+
+def computeAngles(p, visible):
+    rad = [math.atan2((v[0] - p[0]), (p[1]) - v[1]) for v in visible]
+    degrees = [math.degrees(r) for r in rad]
+    return [d if d >= 0 else d + 360 for d in degrees]
+
+
+def argsort(seq):
+    return sorted(range(len(seq)), key=seq.__getitem__)
+
+
 grid = []
 while True:
     try:
@@ -9,34 +37,31 @@ while True:
         break
 
 asteroids = []
-
 for y, line in enumerate(grid):
     for x, c in enumerate(line):
         if c == '#':
             asteroids.append((x, y))
 
-
-def getVisibleAsteroids(a, asteroids):
-    pos = set()
-    for b in asteroids:
-        if a == b:
-            pass
-        else:
-            rel = (a[0] - b[0], a[1] - b[1])
-            length = math.sqrt(rel[0] * rel[0] + rel[1] * rel[1])
-            e = (rel[0] / length, rel[1] / length)
-            pos.add((round(e[0], 6), round(e[1], 6)))
-    return pos
-
-
 relative_pos = {}
 for a in asteroids:
     relative_pos[a] = getVisibleAsteroids(a, asteroids)
 
+position = max(relative_pos, key=lambda k: len(relative_pos[k]))
+print("station: " + str(position))
 
-distances = {}
-for k in relative_pos:
-    distances.append(len(relative_pos[k]))
-
-
-print(max(distances))
+visible = relative_pos[position]
+while len(visible) > 0:
+    angles = computeAngles(position, visible)
+    idx = argsort(angles)
+    for count, i in enumerate(idx):
+        print((1 + count, angles[i], visible[i]))
+    count = 0
+    count += len(idx)
+    if count > 200:
+        print("The 200th asteroid to be vaporized is at " +
+              str(visible[idx[200]]))
+        break
+    else:
+        print("next round")
+        asteroids = [a for a, i in enumerate(asteroids) if i not in idx]
+        visible = getVisibleAsteroids(position, asteroids)
